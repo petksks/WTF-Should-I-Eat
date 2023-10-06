bd75939b993f0013382381c0d4dabd1640093e46
petksks <115067926+petksks@users.noreply.github.com>
2023-10-06 13:42:30 +0200

    fix ingredients logic and button in mobile view

--- components/page.tsx
+++ components/page.tsx
@@ -1,27 +1,36 @@
 "use client";
 import { useEffect, useState, useCallback } from 'react';
 
-const MealFetcher = () => {
-    interface Meal {
-        strMeal: string;
-        strMealThumb: string;
-        strInstructions: string;
-        strIngredients: string;
-    }
+interface Meal {
+    strMeal: string;
+    strMealThumb: string;
+    strInstructions: string;
+    strIngredients: string[];
+}
 
+const MealFetcher = () => {
     const [meal, setMeal] = useState<Meal | undefined>();
     const [view, setView] = useState('instructions');
-
-    const toggleView = () => {
-        setView(view === 'instructions' ? 'ingredients': 'instructions')
-    }
+    const [buttonClicked, setButtonClicked] = useState(false);
 
     const fetchMeal = useCallback(async () => {
         const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
         const data = await response.json();
-        console.log(data);
-        setMeal(data.meals[0]);
+        const mealData = data.meals[0];
+        const ingredients = [];
+    
+        for (let i = 1; i <= 20; i++) {
+            const ingredient = mealData[`strIngredient${i}`];
+            if (ingredient) {
+                ingredients.push(ingredient);
+            }
+        }
+    
+        mealData.strIngredients = ingredients;
+        setMeal(mealData);
+        setButtonClicked(false);
     }, []);
+    
 
     useEffect(() => {
         fetchMeal();
@@ -31,26 +40,29 @@ const MealFetcher = () => {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-4 m-4 bg-gray-100 space-y-4">
-            <h1 className="text-5xl font-extrabold dark.text-white">Wtf should you eat tonight?</h1>
-            <button className="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow" onClick={fetchMeal}>
-                <div className="absolute inset-0 w-3 bg-amber-400 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
-                <span className="relative text-black group-hover:text-white">No thank you.</span>
+            <h1 className="text-5xl font-extrabold dark:text-black">Wtf should you eat tonight?</h1>
+            <button 
+                style={{ backgroundColor: buttonClicked ? 'yellow' : 'white' }}
+                className="group relative h-12 w-48 overflow-hidden rounded-lg text-lg shadow" 
+                onClick={() => {
+                    fetchMeal();
+                    setButtonClicked(true);
+                }}>
+                <span className="relative text-black group-hover:text-black">No thank you.</span>
             </button>
-            
+
             <h2 className="text-2xl font-bold mb-4">{meal.strMeal}</h2>
             <img className="mb-4" src={meal.strMealThumb} alt={meal.strMeal} />
             <div>
                 <button 
-                    className={`group relative h-12 w-48 overflow-hidden rounded-lg text-lg shadow 
-                        ${view === 'ingredients' ? 'bg-amber-400 text-white' : 'bg-white text-black'}`}
+                    className={`group relative h-12 w-48 overflow-hidden rounded-lg text-lg shadow ${view === 'ingredients' ? 'bg-amber-400 text-white' : 'bg-white text-black'}`}
                     onClick={() => setView('ingredients')}
                 >
                     <div className={`absolute inset-0 w-3 bg-amber-400 transition-all duration-[250ms] ease-out group-hover:w-full`}></div>
                     <span className="relative group-hover:text-white">Ingredients</span>
                 </button>
                 <button 
-                    className={`group relative h-12 w-48 overflow-hidden rounded-lg text-lg shadow 
-                        ${view === 'instructions' ? 'bg-amber-400 text-white' : 'bg-white text-black'}`}
+                    className={`group relative h-12 w-48 overflow-hidden rounded-lg text-lg shadow ${view === 'instructions' ? 'bg-amber-400 text-white' : 'bg-white text-black'}`}
                     onClick={() => setView('instructions')}
                 >
                     <div className={`absolute inset-0 w-3 bg-amber-400 transition-all duration-[250ms] ease-out group-hover:w-full`}></div>
@@ -59,16 +71,15 @@ const MealFetcher = () => {
             </div>
             {view === 'instructions' ? (
                 <ol className="list-decimal list-inside">
-                    {meal.strInstructions.split('. ').map((step: string, index: number) => (
+                    {meal.strInstructions.split('. ').map((step, index) => (
                         step && <li key={index} className="mb-2">{step}.</li>
                     ))}
                 </ol>
             ) : (
                 <ol className="list-decimal list-inside">
-                    {/* Assuming you will have a way to parse ingredients into an array */}
-                    {/* {meal.strIngredients.map((ingredient: string, index: number) => (
+                    {meal.strIngredients.map((ingredient, index) => (
                         <li key={index} className="mb-2">{ingredient}</li>
-                    ))} */}
+                    ))}
                 </ol>
             )}
         </div>
